42dc533c42408d14f414fa6afb68df460c7075fd
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2022-01-31 15:56:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+song_dynamite="";
+song_levitating="";
+
 function setup() {
-    canvas = createCanvas(600,560);
+    canvas = createCanvas(650,500);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(560,560);
     video.hide();
+}
+
+function preload() {
+  song_dynamite=loadSound("dynamite.mp3");
+  song_levitating=loadSound("levitating.mp3");  
+}
+
+function draw() {
+    image(video,0,0,650,500);
 }
