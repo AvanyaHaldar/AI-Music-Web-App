7f094b9756d0f45253a975d1ced204f9c30252cf
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2022-02-02 15:47:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-song_dynamite="";
-song_levitating="";
+song_dynamite = "";
+song_levitating = "";
+
+right_wristX = 0;
+right_wristY = 0;
+
+left_wristX = 0;
+left_wristY = 0;
 
 function setup() {
-    canvas = createCanvas(650,500);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
+  canvas = createCanvas(650, 500);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  posenet = ml5.poseNet(video, modelLoaded);
+  posenet.on("pose", getPoses);
+}
+
+function modelLoaded() {
+  console.log("Posenet Is Initialized");
+}
+
+function getPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+
+    right_wristX = results[0].pose.rightWrist.x;
+    right_wristY = results[0].pose.rightWrist.y;
+    console.log("Right Wrist X=" + right_wristX + " Right Wrist Y=" + right_wristY);
+
+    left_wristX = results[0].pose.leftWrist.x;
+    left_wristY = results[0].pose.leftWrist.y;
+    console.log("Left Wrist X=" + left_wristX + " Left Wrist Y=" + left_wristY);
+  }
 }
 
 function preload() {
-  song_dynamite=loadSound("dynamite.mp3");
-  song_levitating=loadSound("levitating.mp3");  
+  song_dynamite = loadSound("dynamite.mp3");
+  song_levitating = loadSound("levitating.mp3");
 }
 
 function draw() {
-    image(video,0,0,650,500);
+  image(video, 0, 0, 650, 500);
 }
